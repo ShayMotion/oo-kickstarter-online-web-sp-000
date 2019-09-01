e3d9854d4a1e99bbe2f2d2933180c3c697fe55b7
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-09-01 19:51:18 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,7 +1,13 @@
 class Project
-  attr_accessor :backer 
-end
+  attr_accessor :title, :backers
 
-def initialize
-  @backed projects = backer 
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    backer.back_project(self) unless backer.backed_projects.include?(self)
+  end
 end
